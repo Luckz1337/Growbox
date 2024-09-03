c411b09e7fe0137ef172bb3e98a30919aa54e376
Luckz1337 <82731240+Luckz1337@users.noreply.github.com>
2024-09-03 23:33:55 +0200

    Create update.py ex

---
+++ ota/update.py
@@ -0,0 +1,9 @@
+from ota import OTAUpdater
+from wifi_config import SSID, PASSWORD
+
+def check_for_updates():
+    firmware_url = "https://raw.githubusercontent.com/Luckz1337/Growbox/"
+    ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+    ota_updater.download_and_install_update_if_available()
+
+check_for_updates()
